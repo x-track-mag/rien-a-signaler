65bb04621a756dc4c0e9f1230bd42106195c752d
zipang <christophe.Desguez@gmail.com>
2021-11-29 20:55:57 +0100

    Add cloudinary loader for next/images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+	images: {
+		// @see https://nextjs.org/docs/api-reference/next/image#built-in-loaders
+		loader: "cloudinary",
+		path: "https://res.cloudinary.com/x-track/"
+	},
 	experimental: {
 		urlImports: ["https://firebasestorage.googleapis.com"]
 	}

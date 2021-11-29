59cb4d56ec41f5cf6f136153f1a87ff975a31cf7
zipang <christophe.Desguez@gmail.com>
2021-11-29 21:04:52 +0100

    Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,47 @@
-import { Heading, VStack } from "@chakra-ui/layout";
-import { withContainerLayout } from "src/layouts/ContainerLayout";
+import ErrorMessage from "components/base/ErrorMessage";
+import { Paragraph, Title } from "components/base/Typography";
+import { ImageGallery } from "components/ImageGallery";
+import HeroSection from "components/sections/Hero";
+import { loadJSONContent } from "lib/utils/JSON";
+import path from "path";
+import { withFullscreenLayout } from "src/layouts/FullscreenLayout";
 
-const IndexPage = () => (
-	<VStack bg="white" mt={6} p={4} boxShadow={10} height="50vh">
-		<Heading>
-			<i>Introducing</i> THE NEXT FIRESTORE STARTUP TEMPLATE
-		</Heading>
-	</VStack>
+/**
+ * @typedef I18nLabel
+ * @property {String} [en] Text in English
+ * @property {String} [fr] Text in French
+ */
+
+/**
+ * @typedef IndexPageProps
+ * @property {I18nLabel} title
+ * @property {String} text
+ * @property {Object} catalog
+ * @property {String} error
+ */
+
+export async function getStaticProps() {
+	const contentPath = path.join(process.cwd(), "_content/exhibition.json");
+	const props = await loadJSONContent(contentPath);
+	return { props };
+}
+
+/**
+ * Home page
+ * @param {IndexPageProps} props
+ * @returns {JSX.Element}
+ */
+const IndexPage = ({ title, text, catalog, error }) => (
+	<HeroSection>
+		{error && <ErrorMessage error={error} />}
+		<Title title={title.en}>{title.fr}</Title>
+		<Paragraph>{text}</Paragraph>
+		<ImageGallery
+			mt={4}
+			pictures={catalog.map(({ images }) => images[0])}
+			height="25vh"
+		/>
+	</HeroSection>
 );
 
-export default withContainerLayout(IndexPage, true);
+export default withFullscreenLayout(IndexPage);

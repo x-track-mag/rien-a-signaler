350c4b574bb843a57e8ff8d5fd286e891695a46a
zipang <christophe.Desguez@gmail.com>
2021-11-29 21:00:46 +0100

    Define the basic typographic styles needed

--- src/components/base/Typography.tsx
+++ src/components/base/Typography.tsx
@@ -1,9 +1,25 @@
-import { Code, Text } from "@chakra-ui/layout";
+import { Heading, Text } from "@chakra-ui/layout";
 
-export const Paragraph = ({ children }) => <Text fontSize="1rem">{children}</Text>;
-
-export const ErrorMessage = ({ children }) => (
-	<Code color="red" fontWeight="bold">
+/**
+ * The main title (rendered as h2)
+ * @param propsWithChildren
+ */
+export const Title = ({ children, ...moreProps }) => (
+	<Heading
+		as="h2"
+		fontSize="3rem"
+		lineHeight={2}
+		fontStyle="italic"
+		letterSpacing="0.2rem"
+		userSelect="all"
+		{...moreProps}
+	>
 		{children}
-	</Code>
+	</Heading>
+);
+
+export const Paragraph = ({ children, ...moreProps }) => (
+	<Text fontSize="1.1rem" mt={1} mb={1} lineHeight="1.5rem" {...moreProps}>
+		{children}
+	</Text>
 );

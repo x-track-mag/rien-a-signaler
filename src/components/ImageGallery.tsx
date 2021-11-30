fc16890dcdcf3e54787e9185989f78d886a1e770
zipang <christophe.Desguez@gmail.com>
2021-11-30 20:07:30 +0100

    Adjust content parsing

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,4 +1,5 @@
 import { SimpleGrid } from "@chakra-ui/layout";
+import Link from "./base/Links";
 import { CloudinaryImage } from "./CloudinaryImage";
 
 /**
@@ -6,21 +7,21 @@ import { CloudinaryImage } from "./CloudinaryImage";
  * @param {ImageGallery} props
  * @returns {JSX.Element}
  */
-export const ImageGallery = ({ pictures = [], ...moreProps }) => (
+export const ImageGallery = ({ catalog = [], ...moreProps }) => (
 	<SimpleGrid
 		className="image-gallery"
 		columns={{ sm: 1, md: 5 }}
-		templateColumns={{ sm: "1fr", md: `repeat(${pictures.length}, 1fr)` }}
+		templateColumns={{ sm: "1fr", md: `repeat(${catalog.length}, 1fr)` }}
 		gap={{ sm: 4, md: 2 }}
 		{...moreProps}
 	>
-		{pictures.map(({ src, alt, width, height }, i) => (
-			<CloudinaryImage
-				key={`img-gallery-${i}`}
-				src={src}
-				alt={alt}
-				ratio={width / height}
-			/>
-		))}
+		{catalog.map(({ id, images }, i) => {
+			const { src, alt, width, height } = images[0]; // Take the first picture
+			return (
+				<Link key={`img-gallery-${i}`} href={`/catalog/${id}`}>
+					<CloudinaryImage src={src} alt={alt} ratio={width / height} />
+				</Link>
+			);
+		})}
 	</SimpleGrid>
 );

5328f491a48b04e65d54a3c3d65d3658181f7dfa
zipang <christophe.Desguez@gmail.com>
2021-12-01 12:49:11 +0100

    Add snipcartApiKey and fix the required variables check

--- src/GlobalEnvironment.ts
+++ src/GlobalEnvironment.ts
@@ -3,48 +3,48 @@
  * @property {String} siteName Used to build each page's title
  * @property {String} siteUrl Base URL (home page)
  * @property {String} siteDescription Used in the Head
+ * @property {String} snipcartApiKey Used in the Head
  */
-type GlobalEnvironment = {
+export type GlobalEnvironment = {
 	siteName: string;
 	siteUrl: string;
 	siteDescription: string;
+	snipcartApiKey: string;
 };
 
 const REQUIRED_ENVIRONMENT_VARS = [
 	"NEXT_PUBLIC_SITE_URL",
 	"NEXT_PUBLIC_SITE_NAME",
-	"NEXT_PUBLIC_SITE_DESCRIPTION"
+	"NEXT_PUBLIC_SITE_DESCRIPTION",
+	"NEXT_PUBLIC_SNIPCART_API_KEY"
 ];
 
 /**
  * Load the global environment variables
- * @returns {GlobalEnvironment}
+ * @param {String} [varName] Name of a specific value to extract
+ * @returns {GlobalEnvironment|string}
  * @throws {Error} if one of the required var is not found
  */
-export const loadEnv = (): GlobalEnvironment => {
-	if (typeof window !== undefined) {
-		// Public environment variables are inlined on the client side !
-		return {
-			siteName: process.env.NEXT_PUBLIC_SITE_NAME as string,
-			siteUrl: process.env.NEXT_PUBLIC_SITE_URL as string,
-			siteDescription: process.env.NEXT_PUBLIC_SITE_DESCRIPTION as string
-		};
+export const loadEnv = (varName?: string): GlobalEnvironment | string => {
+	if (typeof window === undefined) {
+		// Check on the server side only
+		// that all the required environment variables are present
+		REQUIRED_ENVIRONMENT_VARS.forEach((varName) => {
+			if (process.env[varName] === undefined) {
+				throw new Error(`Required environment variable '${varName}' was not found.
+Check your '.env' file or create the requested secret in your Vercel project settings.
+${JSON.stringify(process.env, null, "\t")}`);
+			}
+		});
 	}
 
-	// Server Side
-	const env = process.env;
-
-	// Check that each environment variable is present
-	REQUIRED_ENVIRONMENT_VARS.forEach((varName) => {
-		if (env[varName] === undefined) {
-			throw new Error(`Required environment variable '${varName}' was not found.
-Check your '.env' file or create the secret in your Vercel project settings.`);
-		}
-	});
-
-	return {
-		siteName: env.NEXT_PUBLIC_SITE_NAME as string,
-		siteUrl: env.NEXT_PUBLIC_SITE_URL as string,
-		siteDescription: env.NEXT_PUBLIC_SITE_DESCRIPTION as string
+	// Note : public environment variables (prefixed with NEXT_PUBLIC) are inlined on the client side !
+	const env = {
+		siteName: process.env.NEXT_PUBLIC_SITE_NAME as string,
+		siteUrl: process.env.NEXT_PUBLIC_SITE_URL as string,
+		siteDescription: process.env.NEXT_PUBLIC_SITE_DESCRIPTION as string,
+		snipcartApiKey: process.env.NEXT_PUBLIC_SNIPCART_API_KEY as string
 	};
+
+	return varName ? env[varName] : env;
 };

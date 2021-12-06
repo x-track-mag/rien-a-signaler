b76076d8aba3f0c93f0718f1c765accd1f623faf
zipang <christophe.Desguez@gmail.com>
2021-12-06 17:10:07 +0100

    ...moreProps

--- src/components/CloudinaryImage.tsx
+++ src/components/CloudinaryImage.tsx
@@ -1,19 +1,25 @@
 import { AspectRatio } from "@chakra-ui/layout";
+import { HTMLChakraProps } from "@chakra-ui/system";
 import Image from "next/image";
 
-type CloudinaryImageProps = {
+interface CloudinaryImageProps extends HTMLChakraProps<"div"> {
 	src: string;
 	alt: string;
 	ratio: number;
-};
+}
 
 /**
  * Wrap the responsive image inside an aspect ratio (width/height)
  * @param {CloudinaryImageProps} props
  * @returns {JSX.Element}
  */
-export const CloudinaryImage = ({ src, alt, ratio = 1.25 }: CloudinaryImageProps) => (
-	<AspectRatio ratio={ratio}>
+export const CloudinaryImage = ({
+	src,
+	alt,
+	ratio = 1.25,
+	...moreProps
+}: CloudinaryImageProps) => (
+	<AspectRatio ratio={ratio} {...moreProps}>
 		<Image src={src} layout="fill" alt={alt} />
 	</AspectRatio>
 );

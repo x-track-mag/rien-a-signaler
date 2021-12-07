5af20d707877eec6267d888d8e58bd8dbd5c106b
zipang <christophe.Desguez@gmail.com>
2021-12-07 15:32:24 +0100

    Hero section with fluid container

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,12 +1,11 @@
 import { Box } from "@chakra-ui/layout";
-import { Center } from "@chakra-ui/react";
 import Container from "components/base/Container";
 
 const HeroSection = ({ children }) => (
-	<Box as="section" className="hero-section" height="100vh">
-		<Center height="100%" pt={6} pb={6} width="100%">
-			<Container>{children}</Container>
-		</Center>
+	<Box as="section" className="hero-section" height="100vh" width="100%">
+		<Container pt={16} pb={8} fluid={true}>
+			{children}
+		</Container>
 	</Box>
 );
 
